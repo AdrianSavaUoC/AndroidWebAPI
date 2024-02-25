4d3b3f9bc2df6290fa1bc10065beb01af094d0b6
AdrianSavaUoC <adi.sava@gmail.com>
2024-02-25 16:43:14 +0000

    Add internet permission Connect to the advice API Extract advice from JSON Get the first word from the advice Create Advice Data class to store the retrieved advice Use advice object to display its data Store new advice in adviceList

--- app/src/main/java/ac/uk/chester/j101418/AdviceActivity.kt
+++ app/src/main/java/ac/uk/chester/j101418/AdviceActivity.kt
@@ -9,6 +9,10 @@ import androidx.activity.result.contract.ActivityResultContracts
 class AdviceActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAdviceBinding
+    private lateinit var id: String
+    private lateinit var advice: String
+
+    private val adviceList = mutableListOf<AdviceData>()
 
     private val resultLauncher = registerForActivityResult (ActivityResultContracts.StartActivityForResult() ) {
             result ->
@@ -19,6 +23,7 @@ class AdviceActivity : AppCompatActivity() {
         resultLauncher.launch(intent)
     }
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -26,9 +31,27 @@ class AdviceActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        val dummyId = intent.getStringExtra("id")
         val dummy = intent.getStringExtra("advice")
 
-        binding.tvAdviceView.text = dummy
+
+        if (dummyId != null) {
+            id = dummyId
+        }
+        if (dummy != null) {
+            advice = dummy
+        }
+
+        val newAdvice = AdviceData(id, advice)
+        adviceList.add(newAdvice)
+
+
+        val words = dummy?.split(Regex("[ ,?;.:!]+"))?.toMutableList()
+        val firstWord = words?.firstOrNull()
+
+        binding.tvAdviceView.text = firstWord
+
+        binding.dummy.text = newAdvice.advice
 
         binding.buttonHome.setOnClickListener {
             loadMaintivity()
--- app/src/main/java/ac/uk/chester/j101418/MainActivity.kt
+++ app/src/main/java/ac/uk/chester/j101418/MainActivity.kt
@@ -9,22 +9,90 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.activity.result.contract.ActivityResultContracts
+import org.json.JSONObject
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.Scanner
+
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var theQuerry: String
+    private lateinit var transferText : String
+
+
 
     private val resultLauncher = registerForActivityResult (ActivityResultContracts.StartActivityForResult() ) {
             result ->
     }
 
-    private fun loadAdviceAcrivity (advice: String) {
+    private fun loadAdviceAcrivity (id: String, advice: String) {
 
         val intent = Intent(this, AdviceActivity::class.java)
         intent.putExtra("advice", advice)
+        intent.putExtra("id", id)
         resultLauncher.launch(intent)
     }
+
+    fun getAdvice () {
+        fetchData("https://api.adviceslip.com/advice")
+    }
+
+    private fun processAdviceJson(jsonString : String) : AdviceData {
+        return try {
+            val jsonObject = JSONObject(jsonString)
+            val slipObject = jsonObject.getJSONObject("slip")
+            val id = slipObject.getInt("id").toString()
+            val advice = slipObject.getString("advice")
+
+            val newAdvice = AdviceData(id, advice)
+
+            newAdvice
+
+        } catch (e: Exception) {
+            return AdviceData("","")
+        }
+    }
+
+    private fun fetchData(urlString: String) {
+        val thread = Thread {
+            try {
+                val url = URL(urlString)
+                val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+                connection.connectTimeout = 3000
+                connection.readTimeout = 3000
+                connection.requestMethod = "GET"
+                connection.connect()
+
+                val responseCode = connection.responseCode
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+
+                    val scanner = Scanner(connection.inputStream).useDelimiter("\\A")
+                    val text = if (scanner.hasNext()) scanner.next() else ""
+
+                    val adviceData = processAdviceJson(text)
+
+                    loadAdviceAcrivity(adviceData.id, adviceData.advice)
+                }
+            }
+            catch (e: IOException) {
+                updateTextView("An error has occurred while retrieving data from the server. $e")
+            }
+        }
+        thread.start()
+    }
+
+
+    private fun updateTextView(text: String) {
+        runOnUiThread {
+            binding.textView.text = text
+        }
+    }
+
+
 
 
 
@@ -38,7 +106,6 @@ class MainActivity : AppCompatActivity() {
         val choiceRange = mutableListOf("Advice", "Quote")
         val adapter = ArrayAdapter(this, R.layout.simple_spinner_item, choiceRange)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
         binding.spFirstChoice.adapter = adapter
 
         binding.spFirstChoice.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
@@ -64,7 +131,14 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.buttonGO.setOnClickListener {
-            loadAdviceAcrivity("dummy")
+
+            if (theQuerry == "Advice") {
+                getAdvice()
+            }
+            if (theQuerry == "Quote") {
+                loadAdviceAcrivity("118","dummyQuote! is the first word in this dummy sentence")
+            }
+
         }
 
 
